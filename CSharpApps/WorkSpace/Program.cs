f9c6146602657b0fe0635721c6acef1ef5bdb54d
Fatih Soysal <fatihsoysal@outlook.com>
2021-08-28 13:48:34 +0300

    Create WorkSpace!

---
+++ CSharpApps/WorkSpace/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WorkSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba WorkSpace!");
+            Console.WriteLine("Bu ilk uygulamamız!");
+            Console.ReadLine();
+        }
+    }
+}
