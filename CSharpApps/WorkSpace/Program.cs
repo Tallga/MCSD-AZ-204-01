755f1687786e5768b2ff97d9df88cce6468a0a62
Fatih Soysal <fatihsoysal@outlook.com>
2021-08-29 10:32:06 +0300

    welcomeMessage added.

--- CSharpApps/WorkSpace/Program.cs
+++ CSharpApps/WorkSpace/Program.cs
@@ -12,12 +12,12 @@ namespace WorkSpace
             int accountBalance = 99;
             bool isLogin = true;
             bool isCompleted = false;
+            string welcomeMessage = "WorkSpace uygulamasına hoşgeldiniz.";
 
 
             Console.WriteLine(1000 * 1.92);
             Console.WriteLine(money * 1.92);
             Console.WriteLine(money * 1.92);
-
 
             Console.WriteLine(message);
             Console.WriteLine(message);
